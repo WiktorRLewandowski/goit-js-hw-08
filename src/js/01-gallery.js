fcbb3ad3b208fa38560996b59617ee182b364f48
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-08 15:41:17 +0200

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+// creating clunky markup
+const galleryItem = [];
+galleryItems.forEach((item) => {
+  let li = document.createElement("li");
+  let img = document.createElement("img");
+  let anchor = document.createElement("a");
+  let url = item.preview;
+  let alt = item.description;
+  let zoom = item.original;
+  li.classList.add("gallery__item");
+  anchor.classList.add("gallery__link");
+  img.classList.add("gallery__image");
+  anchor.setAttribute("href", zoom);
+  img.setAttribute("src", url);
+  img.setAttribute("alt", alt);
+  img.setAttribute("title", alt);
+  img.dataset.source = zoom;
+  li.append(anchor);
+  anchor.append(img);
+  galleryItem.push(li);
+});
+// adding new elements to DOM
+galleryEl.append(...galleryItem);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
