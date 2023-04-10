11cf878b016f1e9ad3167adbac227e316441d6b0
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-11 00:24:44 +0200

    adding vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,27 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
 
 // creating clunky markup
 const galleryItem = [];
-galleryItems.forEach((item) => {
-  let li = document.createElement("li");
-  let img = document.createElement("img");
-  let anchor = document.createElement("a");
+galleryItems.forEach(item => {
+  let li = document.createElement('li');
+  let img = document.createElement('img');
+  let anchor = document.createElement('a');
   let url = item.preview;
   let alt = item.description;
   let zoom = item.original;
-  li.classList.add("gallery__item");
-  anchor.classList.add("gallery__link");
-  img.classList.add("gallery__image");
-  anchor.setAttribute("href", zoom);
-  img.setAttribute("src", url);
-  img.setAttribute("alt", alt);
-  img.setAttribute("title", alt);
+  li.classList.add('gallery__item');
+  anchor.classList.add('gallery__link');
+  img.classList.add('gallery__image');
+  anchor.setAttribute('href', zoom);
+  img.setAttribute('src', url);
+  img.setAttribute('alt', alt);
+  img.setAttribute('title', alt);
   img.dataset.source = zoom;
   li.append(anchor);
   anchor.append(img);
@@ -30,6 +30,6 @@ galleryItems.forEach((item) => {
 // adding new elements to DOM
 galleryEl.append(...galleryItem);
 
-const lightbox = new SimpleLightbox(".gallery a", {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
