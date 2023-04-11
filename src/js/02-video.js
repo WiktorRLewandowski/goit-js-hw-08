7faf80762476d94acb881c9e2e5bd9abae8d5f43
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-12 00:32:44 +0200

    lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -9,4 +10,17 @@ player.on('play', function () {
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
+});
+
+player.on('timeupdate', throttle((event) => {
+  console.log(event);
+  localStorage.setItem("videoplayer-current-time", event)
+  }, 1000)
+)
+
+const playerTimer = localStorage.getItem("videoplayer-current-time")
+
+player
+.setCurrentTime(JSON.parse(playerTimer))
+  .then(function(seconds) {
 });
