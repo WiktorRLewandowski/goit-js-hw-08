7ab13dca577acdb42c193a945c79e04988e4671a
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-12 20:59:14 +0200

    task-02 done?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,23 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-player.on('timeupdate', throttle((event) => {
-  console.log(event);
-  localStorage.setItem("videoplayer-current-time", event)
+player.on(
+  'timeupdate',
+  throttle(event => {
+    console.log(event.seconds);
+    localStorage.setItem('videoplayer-current-time', event.seconds);
   }, 1000)
-)
+);
 
-const playerTimer = localStorage.getItem("videoplayer-current-time")
+const playerTimer = localStorage.getItem('videoplayer-current-time');
 
-player
-.setCurrentTime(JSON.parse(playerTimer))
-  .then(function(seconds) {
-});
+player.setCurrentTime(playerTimer).then();
