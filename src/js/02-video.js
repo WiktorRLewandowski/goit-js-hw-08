e43a39ae41b7964a39bcc0b188f53d406c733a42
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-13 22:39:59 +0200

    task-03 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const body = document.body;
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+// bardzo za to przepraszam :( wiem, że zła jest przez to kolejność kodu
+const div = document.createElement('div');
+body.append(div);
+div.append(iframe);
+div.style.padding = '3rem';
+div.style.display = 'flex';
+div.style.alignItems = 'center';
+div.style.justifyContent = 'center';
+iframe.style.filter = 'drop-shadow(0 0 3rem)';
+
+// właściwy kod zadania
 player.on(
   'timeupdate',
   throttle(event => {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('button');
+
+form.addEventListener('input', throttle(inputSaver, 500));
+
+function inputSaver(event) {
+  localStorage.setItem(event.target.name, event.target.value);
+}
+
+form.email.value = localStorage.getItem('email');
+form.message.value = localStorage.getItem('message');
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  console.log(`email: ${localStorage.getItem('email')}`);
+  console.log(`message: ${localStorage.getItem('message')}`);
+  localStorage.clear();
+  form.email.value = localStorage.getItem('email');
+  form.message.value = localStorage.getItem('message');
+});
